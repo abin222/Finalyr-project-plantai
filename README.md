e117eb2751f34d6fff2a25a6c381522167599200
abin222 <43913484+abin222@users.noreply.github.com>
2019-04-01 16:09:28 +0530

    initial

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Finalyr-project-plantai
+#naijahacks-fort-mobile
+
+Mobile application codebase for the plant disease recognition artificial inteligence project which I collaborated on as a member of *Team fort* in the *NaijaHacks* hackaton. PlantAI mobile can be build for both android and ios.
+```
+$npm install
+
+// build for andorid
+$ionic cordova build android --release --prod --minifyjs --minifycss --optimizejs
+
+// build for ios
+$ionic cordova build ios --release --prod --minifyjs --minifycss --optimizejs
+```
+
+
+You can also download already built and signed: https://github.com/ahkohd/naijahacks-fort-mobile/blob/master/NaijaHacksPlantAI.apk
---
+++ src/providers/api/api.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ApiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApiProvider {
+
+
+  // https://cors-anywhere.herokuapp.com/
+  private rexApi: string = "https://naijahacks.appspot.com/predict";
+  private googleApis: string = "https://translation.googleapis.com/language/translate/v2";
+  private lang = ["yo"];
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ApiProvider Provider');
+  }
+
+
+  fetchWiki(query: string)
+  {
+    
+    return this.http.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}`);
+  }
+
+
+  postImage(imgUrl)
+  {
+    return this.http.post(this.rexApi, { plant_image:  imgUrl});
+  }
+
+  translate(text)
+  {
+    return this.http.post(this.googleApis, {
+      key: "AIzaSyA5bnuKKsxXeq80REjqEwQVNI7BffcPx6I",
+      q: text,
+      target: "yo"
+    })
+  }
+
+}
